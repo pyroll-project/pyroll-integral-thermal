4ae8600d68342c413842b9f8eabde44e5623f27f
Max Weiner <max.weiner@imf.tu-freiberg.de>
2022-03-03 10:53:41 +0100

    upd report hooks to pyroll 0.4.3

--- pyroll_integral_thermal/report_impls.py
+++ pyroll_integral_thermal/report_impls.py
@@ -4,25 +4,28 @@ from typing import Sequence
 import numpy as np
 from pyroll import RollPass, Transport, Unit
 from pyroll.ui.report import Report, utils
+from pyroll.utils.hookutils import applies_to_unit_types
 
 
-@Report.hookimpl
-def roll_pass_property(roll_pass: RollPass):
-    return [
-        ("temperature change", "{:.2g}".format(roll_pass.temperature_change)),
-        ("temperature change by deformation", "{:.2g}".format(roll_pass.temperature_change_by_deformation)),
-        ("temperature change by contact", "{:.2g}".format(roll_pass.temperature_change_by_contact)),
-    ]
+@Report.hookimpl(specname="unit_properties")
+@applies_to_unit_types(RollPass)
+def pass_properties(unit: RollPass):
+    return {
+        "temperature change": "{:.2g}".format(unit.temperature_change),
+        "temperature change by deformation": "{:.2g}".format(unit.temperature_change_by_deformation),
+        "temperature change by contact": "{:.2g}".format(unit.temperature_change_by_contact),
+    }
 
 
-@Report.hookimpl
-def transport_property(transport: Transport):
-    return [
-        ("temperature change", "{:.2g}".format(transport.temperature_change)),
-        ("temperature change by convection", "{:.2g}".format(transport.temperature_change_by_convection)),
-        ("temperature change by cooling", "{:.2g}".format(transport.temperature_change_by_cooling)),
-        ("temperature change by radiation", "{:.2g}".format(transport.temperature_change_by_radiation)),
-    ]
+@Report.hookimpl(specname="unit_properties")
+@applies_to_unit_types(Transport)
+def transport_properties(unit: Transport):
+    return {
+        "temperature change": "{:.2g}".format(unit.temperature_change),
+        "temperature change by convection": "{:.2g}".format(unit.temperature_change_by_convection),
+        "temperature change by cooling": "{:.2g}".format(unit.temperature_change_by_cooling),
+        "temperature change by radiation": "{:.2g}".format(unit.temperature_change_by_radiation),
+    }
 
 
 @Report.hookimpl
